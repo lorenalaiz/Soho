3262a9421c677b8a5702519180eca4e3a2d90f2b
Lorena Laiz M. da Silva <lorenalmsilva@gmail.com>
2015-08-22 01:50:29 +1000

    lo

--- JS/map.js
+++ JS/map.js
@@ -1,35 +1,70 @@
 var map;
- 
-function initialize() {
-    var latlng = new google.maps.LatLng(-34.044046, 151.133857);
- 
+var idInfoBoxAberto;
+var infoBox = [];
+var markers = [];
+
+function initialize() { 
+    var latlng = new google.maps.LatLng(-34.044046,151.133857 );
+    
     var options = {
-        zoom: 15,
+        zoom: 14,
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
- 
+
     map = new google.maps.Map(document.getElementById("map"), options);
 }
- 
+
 initialize();
 
+function abrirInfoBox(id, marker) {
+    if (typeof(idInfoBoxAberto) == 'number' && typeof(infoBox[idInfoBoxAberto]) == 'object') {
+        infoBox[idInfoBoxAberto].close();
+    }
+
+    infoBox[id].open(map, marker);
+    idInfoBoxAberto = id;
+}
+
 function carregarPontos() {
- 
-    $.getJSON('js/points.json', function(points) {
- 
-        $.each(points, function(index, ponto) {
- 
+    
+    $.getJSON('js/pontos.json', function(pontos) {
+        
+        var latlngbounds = new google.maps.LatLngBounds();
+        
+        $.each(pontos, function(index, ponto) {
+            
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(ponto.Latitude, ponto.Longitude),
                 title: "Meu ponto personalizado! :-D",
-                map: map
+                icon: 'pictures/marcador.png'
             });
- 
+            
+            var myOptions = {
+                content: "<p>" + ponto.Descricao + "</p>",
+                pixelOffset: new google.maps.Size(-150, 0)
+            };
+
+            infoBox[ponto.Id] = new InfoBox(myOptions);
+            infoBox[ponto.Id].marker = marker;
+            
+            infoBox[ponto.Id].listener = google.maps.event.addListener(marker, 'click', function (e) {
+                abrirInfoBox(ponto.Id, marker);
+            });
+            
+            markers.push(marker);
+            
+            latlngbounds.extend(marker.position);
+            
         });
- 
+        
+        var markerCluster = new MarkerClusterer(map, markers);
+        
+        map.fitBounds(latlngbounds);
+        
     });
- 
+    
 }
- 
+
 carregarPontos();
+    
