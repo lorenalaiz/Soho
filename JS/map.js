a9a4c23126a6ca0cc2b382ee8926c5a61c532a2a
Lorena Laiz M. da Silva <lorenalmsilva@gmail.com>
2015-08-23 17:36:57 +1000

    lo
    cor
    formatacao schedule

--- JS/map.js
+++ JS/map.js
@@ -13,6 +13,9 @@ function initialize() {
     };
 
     map = new google.maps.Map(document.getElementById("map"), options);
+	
+	carregarPontos();
+	
 }
 
 initialize();
@@ -28,7 +31,59 @@ function abrirInfoBox(id, marker) {
 
 function carregarPontos() {
     
-    $.getJSON('js/pontos.json', function(pontos) {
+	var locations = [
+		[
+			"Teste 1",
+			-34.0439084,
+			151.1291238,
+			1,
+			"Teste Name 1",
+			"",
+			"Teste Desc 1",
+			"Teste Desc 1.1"
+		],
+		[
+			"Teste 2",
+			-34.035057,
+			151.100356,
+			2,
+			"Teste Name 2",
+			"Teste Name 2.1",
+			"Teste Desc 2",
+			"Teste Desc 2.1"
+		],
+		[
+			"Teste 3",
+			-34.0504425,
+			151.1644733,
+			3,
+			"Teste Name 3",
+			"Teste Name 3.1",
+			"Teste Desc 3",
+			"Teste Desc 3.1"
+		]
+	]
+    
+	var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0], locations[i][6]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+
+	
+    /*$.getJSON('pontos.json', function(pontos) {
         
         var latlngbounds = new google.maps.LatLngBounds();
         
@@ -36,6 +91,7 @@ function carregarPontos() {
             
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(ponto.Latitude, ponto.Longitude),
+				map:map,
                 title: "Meu ponto personalizado! :-D",
                 icon: 'pictures/marcador.png'
             });
@@ -62,9 +118,9 @@ function carregarPontos() {
         
         map.fitBounds(latlngbounds);
         
-    });
+    });*/
     
 }
 
-carregarPontos();
+//carregarPontos();
     
